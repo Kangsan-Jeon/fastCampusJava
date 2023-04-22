b7c0d0a7d6e941844d962720e2edaea8a8f04e2c
Kangsan-Jeon <tks0204@naver.com>
2023-04-22 22:31:03 +0900

    feat(ch01-09)
    feat(chap01-09): add chapter 01~09 code

---
+++ Chapter1/src/ch04/BinaryTest.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        int num = 10;
+        int nNum = 0B1010;  // binary
+        int oNum = 012; // octal
+        int xNum = 0XA; // hexa
+
+        System.out.println(num);
+        System.out.println(nNum);
+        System.out.println(oNum);
+        System.out.println(xNum);
+    }
+}
