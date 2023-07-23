d3b8154c7abbec16b254b58f0999683889534146
Kangsan-Jeon <tks0204@naver.com>
2023-07-23 22:31:37 +0900

    feat(3rd chapter): add chapter 11 sample code

---
+++ Chapter3/src/ch11/CalculatorTest.java
@@ -0,0 +1,17 @@
+package Chapter3.src.ch11;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calc calc = new CompleteCalc();
+
+        int num1 = 10;
+        int num2 = 2;
+
+        System.out.println(calc.add(num1, num2));
+        System.out.println(calc.subtract(num1, num2));
+        System.out.println(calc.times(num1, num2));
+        System.out.println(calc.divide(num1, num2));
+
+        
+    }
+}
